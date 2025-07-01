4740b700b8de787152b8223c4800b202768734cc
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2025-07-01 20:27:43 -0300

    Update action.ts

--- components/cadastroDemanda/action.ts
+++ components/cadastroDemanda/action.ts
@@ -3,21 +3,22 @@
 import * as Demanda from '../../core/CadastrarDemanda'
 
 export async function registrarDemanda(data: any, token: string, isAdmin: boolean) {
-  if (isAdmin) {
+  if (data.id) {
     return await Demanda.editarDemanda(data, token)
   } else {
-    if (data.id) {
-      return await Demanda.editarDemanda(data, token)
-    } else {
-      return await Demanda.criarDemanda(data, token)
-    }
+    return await Demanda.criarDemanda(data, token)
   }
 }
 
-export async function getProximoProtocolo(token:string) {
+
+export async function getProximoProtocolo(token: string) {
   return Demanda.getProximoProtocolo(token)
 }
 
 export async function getSetores(token: string) {
   return await Demanda.listarSetores(token)
+}
+
+export async function getUsuarios(token: string) {
+  return await Demanda.listarUsuarios(token)
 }
